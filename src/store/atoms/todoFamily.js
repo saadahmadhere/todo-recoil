ab764076cc273b9de0cfec759ae4d6ee4569d9b0
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2024-03-31 07:57:44 +0530

    selector family for async calls

--- src/store/atoms/todoFamily.js
+++ src/store/atoms/todoFamily.js
@@ -1,21 +1,35 @@
-import { atomFamily } from 'recoil';
+import axios from 'axios';
+import { atomFamily, selectorFamily } from 'recoil';
 
-const TODOS = [
-	{
-		id: 1,
-		title: 'Go to Gym',
-		description: 'Hit the gym from 7-9',
-	},
-	{
-		id: 2,
-		title: 'Go to eat food',
-		description: 'Eat food from from 9-11',
-	},
-];
+// const TODOS = [
+// 	{
+// 		id: 1,
+// 		title: 'Go to Gym',
+// 		description: 'Hit the gym from 7-9',
+// 	},
+// 	{
+// 		id: 2,
+// 		title: 'Go to eat food',
+// 		description: 'Eat food from from 9-11',
+// 	},
+// ];
+
+// export const todosAtomFamily = atomFamily({
+// 	key: 'todosFamily',
+// 	default: (id) => {
+// 		return TODOS.find((item) => item.id === id);
+// 	},
+// });
 
 export const todosAtomFamily = atomFamily({
-	key: 'todosFamily',
-	default: (id) => {
-		return TODOS.find((item) => item.id === id);
-	},
+	key: 'todosAtomFamily',
+	default: selectorFamily({
+		key: 'todosSelectorFamily',
+		get: (id) => async () => {
+			const res = await axios.get(
+				`https://sum-server.100xdevs.com/todo?id=${id}`
+			);
+			return res.data.todo;
+		},
+	}),
 });
