cc6f4588db2cf09a65eca1ab8d354a729f14bc3c
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2024-04-13 11:10:41 +0530

    useIsOnline

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
+import { useState } from 'react';
 import './App.css';
-import { RecoilRoot, useRecoilState, useRecoilValueLoadable } from 'recoil';
-import { todosAtomFamily } from './store/atoms/todoFamily';
-import useIsOnline from './custom-hooks/useIsOnline';
+import useInterval from './custom-hooks/useInterval';
 
 function App() {
-	return (
-		<RecoilRoot>
-			<Todo id={1} />
-			<Todo id={2} />
-			<Todo id={2} />
-			<Todo id={3} />
-		</RecoilRoot>
-	);
-}
+	const [count, setCount] = useState(0);
 
-function Todo({ id }) {
-	const todo = useRecoilValueLoadable(todosAtomFamily(id));
-	const { isUserOnline } = useIsOnline();
-	console.log(isUserOnline);
-
-	if (todo.state === 'loading') {
-		return <div>Loading...</div>;
-	}
-	if (todo.state === 'hasError') {
-		return <div> backend returned error.</div>;
-	}
-	return (
-		<>
-			{todo.contents.title}
-			{todo.contents.description}
-			<br />
-		</>
-	);
+	useInterval(() => {
+		setCount((c) => c + 1);
+	});
+	return <div>count is {count}</div>;
 }
 
 export default App;
---
+++ src/custom-hooks/useInterval.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+const useInterval = (cb, n) => {
+	useEffect(() => {
+		const id = setInterval(() => {
+			cb();
+		}, n);
+
+		return clearInterval(id);
+	}, [cb, n]);
+	return <div></div>;
+};
+
+export default useInterval;
