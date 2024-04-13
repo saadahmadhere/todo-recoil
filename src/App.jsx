2dc0c80aff5ef071411bd9075f6da3b848f2147e
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2024-04-14 00:57:37 +0530

    use debounce (self try)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react';
 import './App.css';
-import useInterval from './custom-hooks/useInterval';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [name, setName] = useState('');
 
-	useInterval(() => {
-		setCount((c) => c + 1);
-	}, 1000);
-	return <div>count is {count}</div>;
+	// const handleDebounce = (val) => setName(val);
+
+	const debounce = (cb, timer) => {
+		let timerId;
+		return (arg) => {
+			clearTimeout(timerId);
+
+			timerId = setTimeout(() => {
+				cb(arg);
+			}, [timer]);
+		};
+	};
+
+	// const debounceChangeHandler = debounce(() => {}, 500)
+
+	return (
+		<div>
+			<input
+				type='text'
+				value={name}
+				// onChange={(e) => debounceChangeHandler(e.target.value)}
+				onChange={(e) => debounce((val) => setName(val), 500)(e.target.value)}
+			/>
+			<h4>Name is {name}</h4>
+		</div>
+	);
 }
 
 export default App;
